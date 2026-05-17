0220d4ef32f282382635260b6361cea485d122a8
kzw-skcs <kazawa@skclinicalsupport.com>
2026-05-18 06:28:38 +0900

    v4: single file, iPhone fix

---
+++ ep_single/sw.js
@@ -0,0 +1,4 @@
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => e.waitUntil(
+  caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+));
